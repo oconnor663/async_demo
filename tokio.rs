df86b919acb2f9c0cff1e965efe150725378acc6
Jack O'Connor <oconnor663@gmail.com>
2024-08-02 17:30:40 -0700

    tokio.rs

---
+++ tokio.rs
@@ -0,0 +1,19 @@
+use futures::future::join;
+use tokio::time::{sleep, Duration};
+
+async fn foo() {
+    println!("foo start");
+    sleep(Duration::from_secs_f64(2.0)).await;
+    println!("foo end");
+}
+
+async fn bar() {
+    println!("bar start");
+    sleep(Duration::from_secs_f64(2.5)).await;
+    println!("bar end");
+}
+
+#[tokio::main]
+async fn main() {
+    join(foo(), bar()).await;
+}
