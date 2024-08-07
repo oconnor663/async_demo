fbe22e9f6fcc1fcec8432017bdb9312003ea1954
Jack O'Connor <oconnor663@gmail.com>
2024-08-06 20:01:04 -0700

    base foo.rs, join.rs, and first.rs directly on tokio.rs

--- join.rs
+++ join.rs
@@ -1,77 +1,19 @@
-use futures::task::noop_waker_ref;
-use std::cell::Cell;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
-std::thread_local! {
-    static NEXT_WAKE_TIME: Cell<Option<Instant>> = Cell::new(None);
-}
-
-struct SleepFuture {
-    wake_time: Instant,
-}
-
-impl Future for SleepFuture {
-    type Output = ();
-
-    fn poll(self: Pin<&mut Self>, _: &mut Context) -> Poll<()> {
-        let now = Instant::now();
-        if now >= self.wake_time {
-            Poll::Ready(())
-        } else {
-            let next = NEXT_WAKE_TIME.get();
-            if next.is_none() || self.wake_time < next.unwrap() {
-                NEXT_WAKE_TIME.set(Some(self.wake_time));
-            }
-            Poll::Pending
-        }
-    }
-}
-
-fn sleep(seconds: f64) -> SleepFuture {
+async fn sleep(seconds: f64) {
     let duration = Duration::from_secs_f64(seconds);
-    let wake_time = Instant::now() + duration;
-    SleepFuture { wake_time }
+    tokio::time::sleep(duration).await;
 }
 
-enum FooFuture {
-    Start,
-    FirstSleep(SleepFuture),
-    SecondSleep(SleepFuture),
-    End,
-}
-
-impl Future for FooFuture {
-    type Output = ();
-
-    fn poll(mut self: Pin<&mut Self>, context: &mut Context) -> Poll<()> {
-        if let FooFuture::Start = *self {
-            println!("foo start");
-            *self = FooFuture::FirstSleep(sleep(0.5));
-        }
-        if let FooFuture::FirstSleep(sleep_future) = &mut *self {
-            if Pin::new(sleep_future).poll(context).is_pending() {
-                return Poll::Pending;
-            }
-            println!("foo middle");
-            *self = FooFuture::SecondSleep(sleep(1.0));
-        }
-        if let FooFuture::SecondSleep(sleep_future) = &mut *self {
-            if Pin::new(sleep_future).poll(context).is_pending() {
-                return Poll::Pending;
-            }
-            println!("foo end");
-            *self = FooFuture::End;
-            return Poll::Ready(());
-        }
-        unreachable!("polled again after Ready");
-    }
-}
-
-fn foo() -> FooFuture {
-    FooFuture::Start
+async fn foo() {
+    println!("foo start");
+    sleep(0.5).await;
+    println!("foo middle");
+    sleep(1.0).await;
+    println!("foo end");
 }
 
 async fn bar() {
@@ -116,13 +58,7 @@ fn join<F1, F2>(f1: F1, f2: F2) -> JoinFuture<F1, F2> {
     }
 }
 
-fn main() {
-    let mut main_future = Box::pin(join(foo(), bar()));
-    let mut context = Context::from_waker(noop_waker_ref());
-    while main_future.as_mut().poll(&mut context).is_pending() {
-        let next = NEXT_WAKE_TIME.get().expect("someone must want a wakeup");
-        let duration = next.saturating_duration_since(Instant::now());
-        NEXT_WAKE_TIME.set(None);
-        std::thread::sleep(duration);
-    }
+#[tokio::main]
+async fn main() {
+    join(foo(), bar()).await;
 }
