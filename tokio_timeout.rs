bd4d623de67d5e7aa5372d88ff403285e13b7873
Jack O'Connor <oconnor663@gmail.com>
2024-08-07 21:53:06 -0700

    tokio_timeout.rs

---
+++ tokio_timeout.rs
@@ -0,0 +1,60 @@
+use futures::future;
+use rand::prelude::*;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::atomic::{AtomicU64, Ordering::Relaxed};
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+static X: AtomicU64 = AtomicU64::new(0);
+
+async fn work() {
+    let mut rng = rand::thread_rng();
+    let seconds = rng.gen_range(0.0..1.0);
+    tokio::time::sleep(Duration::from_secs_f32(seconds)).await;
+    X.fetch_add(1, Relaxed);
+}
+
+async fn lots_of_work() {
+    let mut futures = Vec::new();
+    for _ in 0..20_000 {
+        futures.push(work());
+    }
+    future::join_all(futures).await;
+}
+
+struct Timeout<F> {
+    sleep: Pin<Box<tokio::time::Sleep>>,
+    inner: Pin<Box<F>>,
+}
+
+impl<F: Future> Future for Timeout<F> {
+    type Output = Option<F::Output>;
+
+    fn poll(mut self: Pin<&mut Self>, context: &mut Context) -> Poll<Self::Output> {
+        if self.sleep.as_mut().poll(context).is_ready() {
+            Poll::Ready(None)
+        } else {
+            match self.inner.as_mut().poll(context) {
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(output) => Poll::Ready(Some(output)),
+            }
+        }
+    }
+}
+
+fn timeout<F: Future>(inner: F, duration: Duration) -> Timeout<F> {
+    Timeout {
+        sleep: Box::pin(tokio::time::sleep(duration)),
+        inner: Box::pin(inner),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let start = Instant::now();
+    timeout(lots_of_work(), Duration::from_secs_f32(0.5)).await;
+    println!("X is {:?}", X);
+    let seconds = (Instant::now() - start).as_secs_f32();
+    println!("{:.3} seconds", seconds);
+}
