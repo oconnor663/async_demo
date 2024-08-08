5218763d8af14771edd1046f79a8aa302d21e92f
Jack O'Connor <oconnor663@gmail.com>
2024-08-08 16:33:24 -0700

    threads_100.rs

---
+++ threads_100.rs
@@ -0,0 +1,19 @@
+use std::io::Write;
+use std::time::Duration;
+
+fn work() {
+    std::thread::sleep(Duration::from_secs(1));
+    print!(".");
+    std::io::stdout().flush().unwrap();
+}
+
+fn main() {
+    let mut threads = Vec::new();
+    for _ in 0..100 {
+        threads.push(std::thread::spawn(work));
+    }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    println!();
+}
