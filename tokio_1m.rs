7e5ed5439b926273547dd796a9f86a9b84433843
Jack O'Connor <oconnor663@gmail.com>
2024-08-08 16:46:45 -0700

    tokio_1m.rs

---
+++ tokio_1m.rs
@@ -0,0 +1,19 @@
+use futures::future;
+use std::io::Write;
+use std::time::Duration;
+
+async fn work() {
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    print!(".");
+    std::io::stdout().flush().unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    let mut futures = Vec::new();
+    for _ in 0..1_000_000 {
+        futures.push(work());
+    }
+    future::join_all(futures).await;
+    println!();
+}
