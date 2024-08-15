74153111f912308a7450819caf0394bfe5c92344
Jack O'Connor <oconnor663@gmail.com>
2024-08-15 11:45:12 -0700

    simplify factorial.rs

--- factorial.rs
+++ factorial.rs
@@ -1,11 +1,8 @@
-use std::future::Future;
-use std::pin::Pin;
-
 async fn factorial(n: u64) -> u64 {
     if n == 0 {
         1
     } else {
-        let recurse: Pin<Box<dyn Future<Output = u64>>> = Box::pin(factorial(n - 1));
+        let recurse = Box::pin(factorial(n - 1));
         n * recurse.await
     }
 }
